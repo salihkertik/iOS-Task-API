a227146d9d85199ddc8e31414ada96e6cdc757e9
Salih Kertik <salihkertik1453@gmail.com>
2023-11-20 23:21:54 +0300

    Added MVC folder structure.

---
+++ iOS-Task-API/Network/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  iOS-Task-API
+//
+//  Created by Salih Kertik on 20.11.2023.
+//
+
+import Foundation
---
+++ iOS-Task-API/Views/TaskTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TaskTableViewCell.swift
+//  iOS-Task-API
+//
+//  Created by Salih Kertik on 20.11.2023.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
